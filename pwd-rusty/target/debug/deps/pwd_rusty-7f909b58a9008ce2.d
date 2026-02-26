42f50711292c9180e442f184907557914e23489e
Kotsunia <95106741+kotsunia@users.noreply.github.com>
2026-02-26 21:27:54 +0000

    Revert "pwd?"
    This reverts commit 639cf5363aaa8dcf4de098416dcd77011b833c69.

---
+++ pwd-rusty/target/debug/deps/pwd_rusty-7f909b58a9008ce2.d
@@ -0,0 +1,5 @@
+/home/neavah/Documents/rs-utls/pwd-rusty/target/debug/deps/pwd_rusty-7f909b58a9008ce2.d: src/main.rs
+
+/home/neavah/Documents/rs-utls/pwd-rusty/target/debug/deps/libpwd_rusty-7f909b58a9008ce2.rmeta: src/main.rs
+
+src/main.rs:
