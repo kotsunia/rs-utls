eab300f244b62d84fb8363ddc3c5b454bfd2e441
Kotsunia <95106741+kotsunia@users.noreply.github.com>
2026-02-26 21:28:53 +0000

    Revert "pwd? (without target)"
    This reverts commit f522913488499b41da079360d90f6351cb0b0b24.

---
+++ pwd-rusty/target/debug/deps/pwd_rusty-6a8839be231052f4.d
@@ -0,0 +1,5 @@
+/home/neavah/Documents/rs-utls/pwd-rusty/target/debug/deps/pwd_rusty-6a8839be231052f4.d: src/main.rs
+
+/home/neavah/Documents/rs-utls/pwd-rusty/target/debug/deps/libpwd_rusty-6a8839be231052f4.rmeta: src/main.rs
+
+src/main.rs:
---
+++ pwd-rusty/target/debug/deps/pwd_rusty-89cd20087d63bc4e.d
@@ -0,0 +1,5 @@
+/home/neavah/Documents/rs-utls/pwd-rusty/target/debug/deps/pwd_rusty-89cd20087d63bc4e.d: src/main.rs
+
+/home/neavah/Documents/rs-utls/pwd-rusty/target/debug/deps/pwd_rusty-89cd20087d63bc4e: src/main.rs
+
+src/main.rs:
